ac85e9a33dd0929f14a706171490718d20875b6c
Raúl Ruano Katz <raururuano1@gmail.com>
2026-07-12 01:32:47 -0600

    Enviar asistencia como JSON

--- src/main.js
+++ src/main.js
@@ -434,7 +434,10 @@ function setupRsvp() {
       await fetch(config.rsvp.endpoint, {
         method: "POST",
         mode: "no-cors",
-        body: new URLSearchParams(payload)
+        headers: {
+          "Content-Type": "text/plain;charset=utf-8"
+        },
+        body: JSON.stringify(payload)
       });
 
       form.reset();
